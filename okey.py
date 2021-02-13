c757ba4aaad9c3f9911075cdf87844cb0eae973b
aaiit <abdellatif.aithammadi@gmail.com>
2021-02-13 23:34:45 +0100

    def preprocess_sentence

--- okey.py
+++ okey.py
@@ -16,6 +16,19 @@ import pickle
 
 # import matplotlib.pyplot as plt
 
+
+def preprocess_sentence(sentence):
+  sentence = sentence.lower().strip()
+  # creating a space between a word and the punctuation following it
+  # eg: "he is a boy." => "he is a boy ."
+  sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+  sentence = re.sub(r'[" "]+', " ", sentence)
+  # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+  sentence = re.sub(r"[^a-zA-Z?.!,]+", " ", sentence)
+  sentence = sentence.strip()
+  # adding a start and an end token to the sentence
+  return sentence
+  
 # data =pd.read_csv("datasets/full_dataset.csv")
 # data= data.sample(frac=1)
 
